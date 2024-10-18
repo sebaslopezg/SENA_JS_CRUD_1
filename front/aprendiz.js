0819dc079a45fca8b12ce962a9e2db1017a39a58
sebas <sebaslg96@gmail.com>
2024-10-18 11:44:22 -0500

    a

--- front/aprendiz.js
+++ front/aprendiz.js
@@ -1,80 +1,64 @@
-//const aprendiz = require("../back/src/aprendiz.js")
+// interacciones con la tabla aprendiz
+let contenido = document.querySelector("#contenido")
+let frmAprendiz = document.querySelector("#frmAprendiz")
+let nombre = document.querySelector("#nombre")
+let apellido = document.querySelector("#apellido")
+let email = document.querySelector("#email")
+let password = document.querySelector("#password")
+//Llamamos el metodo de creacion del modal de bootstrap
+//tener en cuenta que funciona con bootstrap bundle
+const myModal = new bootstrap.Modal(
+  document.getElementById('')
+)
 
-let api = "http://localhost:4100/api/aprendiz/"
-const content = document.getElementById('content')
-const contenidoTabla = document.getElementById('contenidoTabla')
-const frmAprendiz = document.getElementById('frmAprendiz')
-let nombre = document.querySelector('#nombre')
-let apellido = document.querySelector('#apellido')
-let email = document.querySelector('#email')
-let password = document.querySelector('#password')
+let api = "http://localhost:4100/api/aprendiz/";
 
-function listarTodos(){
-    fetch(api + 'listartodos')
-    .then(res=>res.json())
-    .then(data=>printTable(data))
-}
 
-function print(data) {
-    content.innerHTML += `
-    <span><b>ID</b></span>
-    <span><b>Nombre</b></span>
-    <span><b>Apellido</b></span>
-    <span><b>Email</b></span>
-    <span><b>Password</b></span>
-    <br>
-    `
-    for (let i = 0; i < data.datos.length; i++) {
-
-        content.innerHTML += `
-
-            <span>${data.datos[i].id}</span>
-            <span>${data.datos[i].nombre}</span>
-            <span>${data.datos[i].apellido}</span>
-            <span>${data.datos[i].email}</span>
-            <span>${data.datos[i].password}</span>
-            <br>
-
-        `
+function listartodos(){
+  fetch(api+'listartodos')
+  .then((res) => res.json())
+  .then((res) => { 
+  /*  for (let index = 0; index < res.aprendiz.length; index++) {         
+     contenido.innerHTML +=   `${res.aprendiz[index].nombre} ${res.aprendiz[index].apellido} <br>`     
     }
-}
-
-function printTable(data){
-    
-    data.datos.forEach(aprendiz => {
-        console.log(aprendiz.apellido)
-        contenidoTabla.innerHTML += `
-        <tr>
-            <td>${aprendiz.id}</td>
-            <td>${aprendiz.nombre}</td>
-            <td>${aprendiz.apellido}</td>
-            <td>${aprendiz.email}</td>
-        </tr>
-        `
-    });
-
-
-}
-
-frmAprendiz.addEventListener("submit", (e)=>{
-    //Previene el evento por defecto de los formularios que hace submit automatico
-    //evitamos enviar espacios en vacio y controlamos el envio desde el boton
-    e.preventDefault()
-    fetch(api + 'crear',{
-        method: "POST",
-        //Configuramos que la cabecera(header) de peticion lleve una configuracion: contiene un archivo JSON 
-        headers: {
-            "Content-Type": "application/json"
-        },
-        //para serializar un objeto JS a JSON
-        body: JSON.stringify({
-            nombre: nombre.value,
-            apellido: apellido.value,
-            email: email.value,
-            password: password.value
-        })
+ 
+    res.aprendiz.map((aprendiz)=>{
+      contenido.innerHTML +=   `${aprendiz.nombre} ${aprendiz.apellido} <br>`   
+    }) */
+    res.aprendiz.forEach((aprendiz)=>{
+      let fila =   `<tr> <td>${aprendiz.id}</td> <td>${aprendiz.nombre}</td> <td>${aprendiz.apellido}</td>  <td>${aprendiz.email}</td></tr><br>`   
+      contenido.innerHTML = fila
     })
-    .then((res)=>res.json())
-    .then((res)=>M.toast({html: res.mensaje}))
-})
+
+  });
+}
+// envia datos por el formulario, el request lleva una payload que es la data de los formularios, 
+// metodo post
+
+
+
+frmAprendiz.addEventListener("submit", (e) => {
+  // previene el evento por defecto de los formularios que hace submit automatico
+  // evitamos enviar espacios vacios y controlamos el envio desde el boton enviar
+  e.preventDefault();
+  fetch(api + "crear", {
+    method: "POST",
+    // configuramos que la cabecera, header de peticion lleve una configuracion: contiene un archivo json
+    headers: {
+      "Content-Type": "application/json",
+    },
+    //carga o payload del request o peticion, serializar un objeto JS  a JSON
+    body: JSON.stringify({
+      nombre: nombre.value,
+      apellido: apellido.value,
+      email: email.value,
+      password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res.status, res.respuesta);
+      M.toast({ html: `${res.mensaje}` });
+    });
+});
 
